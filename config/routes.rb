5a9138468b2c9444670b3bc27a9985bea2486f27
robo2323 <rob.adam.h@gmail.com>
2018-04-06 11:45:25 +1000

    readme

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   root to: "presets#index"
 
-
-  # resources :users
-
   resources :users, except: [:index]
 
   get 'presets/all', to: 'presets#all'
@@ -14,11 +11,8 @@ Rails.application.routes.draw do
   resources :ratings, only: [:create, :update, :destroy]
 
 
-  # resources :users, only: [:new, :create]
-  # see rails guide for routing, esp. nested routes
-
   get '/login', to: 'session#new' #sign in form
   post '/login', to: 'session#create' #sign in action
   delete '/login', to: 'session#destroy' #sign out
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
